5fef68478c73fc05663046db7ebe8254af142229
Federico Filì <federico.fili@di.unito.it>
2024-11-02 17:39:32 +0100

    split dataset on train and val

--- preprocessing.py
+++ preprocessing.py
@@ -1,38 +1,34 @@
 from library import *
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+def get_data_augmentation():
+    return transforms.Compose([
+        transforms.RandomHorizontalFlip(),  
+        transforms.RandomRotation(10),  
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Variazioni di colore
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))  
+    ])
+
+# transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+transform = get_data_augmentation()
 cifar10 = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
+selected_classes = [0, 1, 3, 5]  
+filtered_data = [(img, label) for i, (img, label) in enumerate(cifar10) if label in selected_classes]
 
-cifar10 = datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
+images = torch.stack([item[0] for item in filtered_data])
+labels = torch.tensor([1 if item[1] == 5 else 0 for item in filtered_data])
 
-train_loader = DataLoader(cifar10, batch_size=32, shuffle=True)
+binary_dataset = TensorDataset(images, labels)
 
-data_iter = iter(train_loader)
-images, labels = next(data_iter)
+dataset_size = len(binary_dataset)
+train_size = int(0.8 * dataset_size)  
+val_size = dataset_size - train_size  
+train_dataset, val_dataset = random_split(binary_dataset, [train_size, val_size])
 
-print(f"Shape of batch images: {images.shape}")
-print(f"Labels: {labels}")
-
-# Redefine dataset 
-dog_class = 5 
-non_dog_classes = [0, 1, 2, 3, 4, 6, 7, 8, 9] 
-
-binary_indices = [i for i, (_, label) in enumerate(cifar10) if label == dog_class or label in non_dog_classes]
-
-binary_dataset = Subset(cifar10, binary_indices)
-binary_dataset.dataset.targets = [1 if cifar10[i][1] == dog_class else 0 for i in binary_indices]
-
-binary_loader = DataLoader(binary_dataset, batch_size=32, shuffle=True)
-
-data_iter = iter(binary_loader)
-images, labels = next(data_iter)
-
-print(f"Shape of batch images: {images.shape}")
-print(f"Binary labels: {labels}")
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
 
+print(f"Training set size: {len(train_dataset)}")
+print(f"Validation set size: {len(val_dataset)}")

8cf2585ac468a1e84356d6748004f5f7b26fb41f
Federico Filì <federico.fili@di.unito.it>
2024-11-02 19:19:42 +0100

    library for dataset

--- library.py
+++ library.py
@@ -5,5 +5,5 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset, random_split
 from torch.utils.data import Subset

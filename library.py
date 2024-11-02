2bf986bbfb6a8f13fd708844ee5a655dcecd2baa
Federico Filì <federico.fili@di.unito.it>
2024-11-02 11:46:13 +0100

    add library for model

---
+++ library.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torch.utils.data import Subset

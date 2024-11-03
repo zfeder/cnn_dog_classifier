679f5b61f62b90577820b1999e3cfb0a839a2f39
Federico Filì <federico.fili@di.unito.it>
2024-11-03 12:58:02 +0100

    add another library

--- library.py
+++ library.py
@@ -1,9 +1,12 @@
+import copy
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from PIL import Image
+import torch.optim as optim
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
+from torch.utils.data import Subset
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, TensorDataset, random_split
-from torch.utils.data import Subset
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
